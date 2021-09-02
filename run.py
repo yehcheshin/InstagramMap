951588ddecaa014febc680ebc479db39b8ff827d
yehcheshin <shin109062639@gapp.nthu.edu.tw>
2021-09-02 15:19:21 +0800

    Add code

--- run.py
+++ run.py
@@ -1,48 +1,40 @@
-from flask import Flask, request, abort
+import os
+from datetime import datetime
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+from flask import Flask, abort, request
+
+# https://github.com/line/line-bot-sdk-python
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(
-    "zSNR6szFA8ZgIVI0ZFMEBUXKFBe/rJGkThJw2/VuVdPnwpHSi3b0K8WK3sUxFsyhbsAV2V2+KNqOp3GZ404Teyp5lfFLTdTeJ5HL6JFHzJ5Ec3+FTqbQ4Tt23UbB4CUwg+xZDRXSsippi7USrMn/2QdB04t89/1O/w1cDnyilFU="
-    )
-handler = WebhookHandler('b0ff8a5576668a0d05bbbf242785dd2f')
+line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
 
 
-@app.route("/callback", methods=['POST'])
+@app.route("/", methods=["GET", "POST"])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    if request.method == "GET":
+        return "Hello Heroku"
+    if request.method == "POST":
+        signature = request.headers["X-Line-Signature"]
+        body = request.get_data(as_text=True)
 
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            abort(400)
 
-    return 'OK'
+        return "OK"
 
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    get_message = event.message.text
 
-
-if __name__ == "__main__":
-    app.run()
+    # Send To Line
+    reply = TextSendMessage(text=f"{get_message}")
+    line_bot_api.reply_message(event.reply_token, reply)
